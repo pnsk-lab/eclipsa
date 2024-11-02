662b2f027460486a4b8fc71a50a0f3f1d82c3198
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-11-02 14:07:31 +0000

    snapshot

--- packages/eclipsa/optimizer/mod.test.ts
+++ packages/eclipsa/optimizer/mod.test.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { buildFile } from './mod.ts'
+import { assertEquals } from '@std/assert'
 
 Deno.test('Main', async () => {
   const isUpdateSnapshot = Deno.args.includes('--update')
@@ -8,8 +9,24 @@ Deno.test('Main', async () => {
   const optimizerDir = path.join(fileURLToPath(import.meta.url), '..')
   const testsDir = path.join(optimizerDir, 'tests')
   for await (const testFile of Deno.readDir(testsDir)) {
+    console.info(`Testing with snapshots... ${testFile.name}`)
+
     const tsx = await Deno.readTextFile(path.join(testsDir, testFile.name))
 
-    buildFile(tsx)
+    const built = await buildFile(tsx)
+    if (!built) {
+      continue
+    }
+    let snapshot = `// ================= ENTRY (${testFile.name}) ==\n${tsx}\n\n`
+    for (const [name, code] of built.client) {
+      snapshot += `// ================= ${name} ==\n${code}\n\n`
+    }
+    const snapShotPath = path.join(optimizerDir, 'snapshots', testFile.name + '.snap')
+
+    if (isUpdateSnapshot) {
+      await Deno.writeTextFile(snapShotPath, snapshot)
+    } else {
+      assertEquals(snapshot, await Deno.readTextFile(snapShotPath))
+    }
   }
 })
--- packages/eclipsa/optimizer/mod.ts
+++ packages/eclipsa/optimizer/mod.ts
@@ -1,20 +1,34 @@
 // @ts-types="@types/babel__core"
 import * as babel from '@babel/core'
 // @ts-types="@types/babel__traverse"
-import type { Visitor } from '@babel/traverse'
+import _traverse from '@babel/traverse'
 import SyntaxJSX from '@babel/plugin-syntax-jsx'
-
+const traverse = _traverse.default
 const { types: t } = babel
 
 export interface Built {
   client: Map<string, string>
 }
-export const buildFile = (source: string): Promise<Built> => {
+export const buildFile = async (source: string): Promise<Built | null> => {
   const parsed = babel.parse(source, {
     plugins: [
       SyntaxJSX.default
     ]
   })
+  if (!parsed) {
+    return null
+  }
 
-  console.log(parsed)
+  traverse(parsed, {
+    Program: {
+      enter(path) {
+        //console.log(path)
+      }
+    }
+  })
+  const client = new Map<string, string>()
+  client.set('a.js', 'console.log(0)')
+  return {
+    client
+  }
 }
