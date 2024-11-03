dd5761e644562f66609a882d12884a773c9132da
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-11-03 14:06:51 +0000

    PoC

---
+++ packages/eclipsa/optimizer/jsx/mod.ts
@@ -0,0 +1,11 @@
+import { babel, t, traverse } from '../babel.ts'
+
+/**
+ * JSX Transpiler (Output maybe optimized)
+ */
+export const transformJSX = (ast: t.File) => {
+  traverse(ast, {
+
+  })
+  return ast
+}
--- packages/eclipsa/optimizer/mod.ts
+++ packages/eclipsa/optimizer/mod.ts
@@ -1,7 +1,8 @@
 import { analyzeImports } from './analyze-import.ts'
-import { babel, t } from './babel.ts'
+import { babel, generate, t } from './babel.ts'
 import SyntaxJSX from '@babel/plugin-syntax-jsx'
 import { analyzeComponents } from './eurl-process.ts'
+import { transformJSX } from './jsx/mod.ts'
 
 export interface Built {
   client: Map<string, string>
@@ -19,6 +20,14 @@ export const buildFile = async (source: string): Promise<Built | null> => {
   const imports = analyzeImports(parsed)
 
   const client = analyzeComponents(parsed, imports)
+
+  for (const [file, code] of client) {
+    client.set(file, generate(transformJSX(babel.parse(code, {
+      plugins: [
+        SyntaxJSX.default
+      ]
+    })!)).code)
+  }
 
   return {
     client
