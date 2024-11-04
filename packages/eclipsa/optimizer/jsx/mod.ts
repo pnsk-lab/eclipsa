aba7622c64b5bdbe1e462df2fcd2c1582f0dcec3
nakasyou <syoutarou1103@outlook.jp>
2024-11-04 19:07:30 +0900

    wip

--- packages/eclipsa/optimizer/jsx/mod.ts
+++ packages/eclipsa/optimizer/jsx/mod.ts
@@ -5,8 +5,31 @@ import { transformJSXElement } from './transform-jsxelement.ts'
  * JSX Transpiler (Output maybe optimized)
  */
 export const transformJSX = (ast: t.File) => {
+  let createTemplateID!: t.Identifier
+  let toInsertBody: t.Statement[] = []
   traverse(ast, {
-    JSXElement: transformJSXElement,
+    Program: {
+      enter(path) {
+        createTemplateID = path.scope.generateUidIdentifier('createTemplate')
+
+        path.node.body.unshift(
+          t.importDeclaration([
+            t.importSpecifier(createTemplateID, t.identifier('createTemplate')),
+          ], t.stringLiteral('@xely/eclipsa/prod-client'))
+        )
+      },
+      exit(path) {
+        path.unshiftContainer('body', toInsertBody)
+      }
+    },
+    JSXElement(path) {
+      const result = transformJSXElement(path, {
+        prodClientIdenifiers: {
+          createTemplate: createTemplateID
+        }
+      })
+      toInsertBody = toInsertBody.concat(result.toInsertBody)
+    },
   })
   return ast
 }
--- packages/eclipsa/optimizer/jsx/transform-jsxelement.ts
+++ packages/eclipsa/optimizer/jsx/transform-jsxelement.ts
@@ -1,12 +1,64 @@
 import { type NodePath, t } from '../babel.ts'
 
-export const transformJSXElement = (path: NodePath<t.JSXElement>) => {
-  const ssrElemId = path.scope.generateUidIdentifier('ssrElem')
-  const templateId = path.scope.generateDeclaredUidIdentifier('template')
-  const elemId = path.scope.generateUidIdentifier('elem')
-  const functionExpr = t.arrowFunctionExpression([ssrElemId], t.blockStatement([
-    t.variableDeclaration('var', [t.variableDeclarator(elemId, t.logicalExpression('??', ssrElemId, t.callExpression(elemId, [])))])
-  ]))
+interface Init {
+  prodClientIdenifiers: {
+    createTemplate: t.Identifier
+  }
+}
+interface Result {
+  toInsertBody: t.Statement[]
+}
 
-  path.replaceWith(functionExpr)
+export const processElement = (path: NodePath<t.JSXElement>): {
+  template: string
+} => {
+  let template = ''
+
+  path.traverse({
+    JSXElement: {
+      enter(path) {
+      }
+    }
+  })
+
+  return {
+    template
+  }
+}
+
+export const transformJSXElement = (path: NodePath<t.JSXElement>, init: Init): Result => {
+  const toInsertBody: t.Statement[] = []
+
+  // Generate Template
+  const templateId = path.scope.generateUidIdentifier('template')
+  toInsertBody.push(
+    t.variableDeclaration('var', [
+      t.variableDeclarator(
+        templateId,
+        t.callExpression(init.prodClientIdenifiers.createTemplate, [
+          t.stringLiteral('<div></div>'),
+        ]),
+      ),
+    ]),
+  )
+
+  const ssrElemId = path.scope.generateUidIdentifier('ssrElem')
+  const elemId = path.scope.generateUidIdentifier('elem')
+  const functionExpr = t.arrowFunctionExpression(
+    [ssrElemId],
+    t.blockStatement([
+      t.variableDeclaration('var', [
+        t.variableDeclarator(
+          elemId,
+          t.logicalExpression('??', ssrElemId, t.callExpression(elemId, [])),
+        ),
+      ]),
+    ]),
+  )
+
+  processElement(path)
+
+  return {
+    toInsertBody,
+  }
 }
