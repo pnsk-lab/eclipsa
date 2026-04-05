186372a598e8f0b54d3ff5324a5f15c89ab15bb0
Shotaro Nakamura <syoutarou1103@outlook.jp>
2026-04-05 18:31:28 +0900

    fix

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite-plus'
-import { eclipsaContent } from '@eclipsa/content/vite'
-import { eclipsa } from 'eclipsa/vite'
+import { eclipsaContent } from '../packages/content/vite.ts'
+import { eclipsa } from '../packages/eclipsa/vite/mod.ts'
 import tailwind from '@tailwindcss/vite'
-import { eclipsaImage } from '@eclipsa/image/vite'
+import { eclipsaImage } from '../packages/image/vite.ts'
 
 export default defineConfig({
   appType: 'custom',
